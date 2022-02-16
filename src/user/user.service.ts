11a2351a0e65de23317814af1b7a271c0c45ad06
Rodrigo Reis de Sousa <32500158+RodrigoRsousa@users.noreply.github.com>
2022-02-16 20:46:41 -0300

    Add files via upload

---
+++ src/user/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+@Injectable()
+
+
+
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.user.create({ data });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+
+
+  
+}
